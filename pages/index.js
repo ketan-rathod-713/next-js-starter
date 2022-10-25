5357283038b6e9b76395a515020434b1b052b91c
ketan <ketanrtd1@gmail.com>
2022-10-25 17:27:29 +0530

    added image

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>3nd year CSE student . DSA++ | NodeJs | Learning++</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
